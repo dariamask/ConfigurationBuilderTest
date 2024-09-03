d8a53fa87cd449ca40b05d30c5c3139130154df0
Darya <ferra@list.ru>
2024-09-04 01:16:50 +0300

    Add project files.

---
+++ ConsoleApp1/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+
+public static class ConfigurationBuilderExtensions
+{
+    public static IConfigurationBuilder AddDirectoryAsConfig(this IConfigurationBuilder builder, string directoryPath)
+    {
+        var data = new Dictionary<string, string>();
+        ProcessDirectory(directoryPath, data, "");
+
+        builder.AddInMemoryCollection(data);
+        return builder;
+    }
+
+    private static void ProcessDirectory(string path, IDictionary<string, string> data, string parentKey)
+    {
+        var directoryInfo = new DirectoryInfo(path);
+        foreach (var file in directoryInfo.GetFiles())
+        {
+            var key = $"{parentKey}{Path.GetFileNameWithoutExtension(file.Name)}";
+            var value = File.ReadAllText(file.FullName);
+            data[key] = value;
+        }
+
+        foreach (var directory in directoryInfo.GetDirectories())
+        {
+            var key = $"{parentKey}{directory.Name}:";
+            ProcessDirectory(directory.FullName, data, key);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    public static void Main()
+    {
+        var configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.AddDirectoryAsConfig("C:\\Repos\\myconfig");
+        var configuration = configurationBuilder.Build();
+
+        Console.WriteLine($"Branch#1 file#1: {configuration["branch1:file1"]}");
+        Console.WriteLine($"Branch#2 file#1: {configuration["branch2:file2"]}");
+        Console.WriteLine($"Branch#2 file#3: {configuration["branch2:file3"]}");
+    }
+}
